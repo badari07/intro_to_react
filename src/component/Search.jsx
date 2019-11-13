9b0bc4c9a941a4177327d34b69bca9579465f295
badari07 <badari0793@gmail.com>
2019-11-13 10:43:48 +0530

    class properties

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -2,12 +2,33 @@ import React from "react";
 import preLoad from "../../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preLoad.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-    </div>
-  </div>
-);
+class Search extends React.Component {
+  state = {
+    searchTerm: ""
+  };
+  handleSearchTermChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+            onChange={this.handleSearchTermChange}
+          />
+        </header>
+        <div>
+          {preLoad.shows.map(show => (
+            <ShowCard key={show.imdbID} {...show} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
