f97f05b376d96a7ebb5c347713802a9a00f7abc1
badari07 <badari0793@gmail.com>
2019-11-15 00:04:27 +0530

    updating test

--- src/__tests__/search.spec.jsx
+++ src/__tests__/search.spec.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import Search from "../component/Search";
+import ShowCard from "../component/ShowCard";
+import preLoad from "../../data.json";
 import { shallow } from "enzyme";
 
 test("search render correctly", () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
+});
+
+test("search should render correct amount of shows based on search term", () => {
+  const searchWord = "black";
+  const component = shallow(<Search />);
+  component.find("input").simulate("change", { target: { value: searchWord } });
+  const showCount = preLoad.shows.filter(
+    show =>
+      `${show.title}${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
+
+test("search should reander correct number of shows ", () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preLoad.shows.length);
 });
--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -8,7 +8,6 @@ class Search extends React.Component {
   };
   handleSearchTermChange = e => {
     this.setState({ searchTerm: e.target.value });
-    e.PreventDefault();
   };
   render() {
     return (
